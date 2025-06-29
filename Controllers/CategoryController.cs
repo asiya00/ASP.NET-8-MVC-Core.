659277db42fed0e6c95434c0196c3a5aaceb0e1c
asiya00 <asiyapathan2000@gmail.com>
2025-06-29 19:20:16 +0530

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Ecommerce.Data;
+using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Category()
+        {
+            List<Category> dbCategoryList = _db.Categories.ToList();
+            return View(dbCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Category");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30, ErrorMessage ="Maximum length of Category Name is 30 characters.")]
+        public string CategoryName { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage ="Display order range is between 1-100.")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
