66085ce7ca15f451dff3b1ac3a3c764a418b8453
asiya00 <asiyapathan2000@gmail.com>
2025-06-29 19:20:16 +0530

    Complete crud operations

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,90 @@
+﻿using Ecommerce.Data;
+using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Category()
+        {
+            List<Category> dbCategoryList = _db.Categories.ToList();
+            return View(dbCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //property related custom error generation
+            if (obj.CategoryName == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CategoryName", "The Name can't be same as Display Order.");
+            }
+
+            //model related error validation
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Category");
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null) {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.Find(id);
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            //property related custom error generation
+            if (obj.CategoryName == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CategoryName", "The Name can't be same as Display Order.");
+            }
+
+            //model related error validation
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Category");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Category");
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ecommerce.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { 
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { ID = 1, CategoryName = "Action", DisplayOrder = 1 },
+                new Category { ID = 2, CategoryName = "Horror", DisplayOrder = 2 },
+                new Category { ID = 3, CategoryName = "Thriller", DisplayOrder = 3 }
+                );
+        }
+    }
+}
